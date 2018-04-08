3891b773a16bda17528189c381b05604e2314c26
pradyot <pradyotprksh4@gmail.com>
2018-04-08 21:46:27 +0530

    Admin Assign Class Teacher

--- app/src/main/java/com/application/pradyotprakash/newattendanceappadmin/AssignClassTeacher.java
+++ app/src/main/java/com/application/pradyotprakash/newattendanceappadmin/AssignClassTeacher.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class AssignClassTeacher extends AppCompatActivity {
 
+    private String subjectId, classValueString, branchValue, semesterValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_class_teacher);
+        subjectId = getIntent().getStringExtra("subjectId");
+        classValueString = getIntent().getStringExtra("classValueString");
+        branchValue = getIntent().getStringExtra("branchValue");
+        semesterValue = getIntent().getStringExtra("semesterValue");
     }
 }
